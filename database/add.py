ee3965343fad9bf25ee0ded1ef046abf6bc03dbd
yadav1133 <160613574+yadav1133@users.noreply.github.com>
2024-02-21 11:54:21 +0530

    add.py

--- database/add.py
+++ database/add.py
@@ -1,5 +1,5 @@
-# with Love @LazyDeveloperr 💘
-# Subscribe YT @LazyDeveloperr - to learn more about this for free...
+# with Love @yadavhackingworld 💘
+# Subscribe YT @yaduvanshi.shubham.991 - to learn more about this for free...
 
 from database.users_chats_db import db
 from pyrogram import Client
